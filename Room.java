2865162ea505936bd3cc60f51a8d7d22be7a55e9
Oussoubi <90727185+Oussoubi@users.noreply.github.com>
2021-11-18 12:51:38 +0000

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,47 +1,29 @@
 package HotelReservationSystem;
 
+import HotelReservationSystem.Reservation;
+
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Room {
-	
-	Scanner roomInput = new Scanner(System.in);
-	Reservation roomType;
-	ArrayList<Reservation> newBooking = new ArrayList<Reservation>();
-	
-	public static void main(String[]args) {
-		Scanner roomInput = new Scanner(System.in);
-		Reservation roomType;
-		ArrayList<Reservation> newBooking = new ArrayList<Reservation>();
-		
-		return roomType.getRoomType(starRating);
-	}
-	
-	public void getRoomType(Scanner starRating) {
-		
-		if(roomInput.equals(5)) {
-			System.out.println("What type of room do you require?\nDeluxe Double (DD), Deluxe Twin (DT), Deluxe Single (DS), Deluxe Family (DF)");
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();
-			newBooking.add(type);			
+	ArrayList<String> room = new ArrayList<String>();
+	private int star, noOfRooms;
+	private String roomType;
+	private boolean isOccupied;
+
+
+	public Room(){
+
+    }
+}
+
+		this.star = star;
+		this.noOfRooms = noOfRooms;
+		this.roomType = roomType;
+		this.isOccupied = isOccupied;
+
+		if(star==5){
+
 		}
-		
-		// 4 star
-		if(roomInput.equals(4)) {
-			System.out.println("What type of room do you require?\nExecutive Double (ED), Executive Twin (ET), Executive Single (ES)");
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();
-			newBooking.add(type);			
-		}
-		
-		// 3 star
-		if(roomInput.equals(3)) {
-			System.out.println("What type of room do you require?\nClassic Double (CD), Classic Twin (CT), Classic Single (CS)");
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();
-			newBooking.add(type);
-			System.out.println(newBooking);
-		}
-		System.out.println(newBooking);
 	}
 }
