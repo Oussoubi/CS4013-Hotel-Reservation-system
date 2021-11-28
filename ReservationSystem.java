3d0eb0744e185bb87c0b366b60c7497ddb1decf9
Niamh Dunlea <90870548+niamhmariedunlea@users.noreply.github.com>
2021-11-28 16:48:37 +0000

    main method to run the ReservationMenu class

--- ReservationSystem.java
+++ ReservationSystem.java
@@ -1,42 +1,15 @@
 package HotelReservationSystem;
 
 import java.io.IOException;
-import java.util.*;
 
-//System that manages and runs the Reservations
-public class RunSystem {
-	//public static void main(String[] args){
-	     //   throws IOException
-	   //{ 
-	     // ReservationMenu load = new ReservationMenu();
-	    //  load.run();
-	  // }
-	//}
+/**
+ * System that manages and runs the Reservations
+ *
+ */
+public class ReservationSystem {
+	public static void main(String[] args) throws IOException {
 
-	public static void getRoomType() {
-
-		Scanner starRating= new Scanner(System.in);
-		System.out.print("How many stars does your ideal hotel have?");
-		int answer = starRating.nextInt();
-		
-		if(starRating.equals(5)) {
-			System.out.println("What type of room do you require?\nDeluxe Double (DD), Deluxe Twin (DT), Deluxe Single (DS), Deluxe Family (DF)");			
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();		
-		}
-		
-		// 4 star
-		if(starRating.equals(4)) {
-			System.out.println("What type of room do you require?\nExecutive Double (ED), Executive Twin (ET), Executive Single (ES)");
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();			
-		}
-		
-		// 3 star
-		if(starRating.equals(3)) {
-			System.out.println("What type of room do you require?\nClassic Double (CD), Classic Twin (CT), Classic Single (CS)");
-			String roomType = roomInput.nextLine().toUpperCase();
-			Reservation type = new Reservation();
-		}
+		ReservationMenu load = new ReservationMenu();
+		load.run();
 	}
 }
