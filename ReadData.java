26157f3a8ce3b6e9aafce8a77d24517f37b546e7
Oussoubi <90727185+Oussoubi@users.noreply.github.com>
2021-11-29 11:58:33 +0000

    Update ReadData.java

--- ReadData.java
+++ ReadData.java
@@ -3,6 +3,7 @@ package HotelReservationSystem;
 import HotelReservationSystem.Reservation;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,18 +12,23 @@ import java.util.List;
 
 public class ReadData{
 
-  public readcsv() throws IOException{
-        String file = "I4Hotels.csv";
-        String line;
-        ArrayList<String> lines = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            while((line = br.readLine()) != null){
-                List values = Arrays.asList(line.split(", "));
-                lines.add(values);
-            }
-            lines.forEach(l -> System.out.println(l));
-        } catch (Exception e){
-            System.out.println(e);
-            }
+  public static void main(String[]args){
+        String file = "C:/Users/Soubz/Documents/Computer Science/Year 2/CS4013/Project/l4Hotels.csv";
+        String line = "";
+        
+        try {
+        	BufferedReader br = new BufferedReader(new FileReader(file));
+        	
+        	while((line = br.readLine()) != null) {
+        		System.out.println(line) ;
+        		  
+        	}
+        	
+        }catch(FileNotFoundException e) {
+        	e.printStackTrace();
+        }catch(IOException e) { 
+        	e.printStackTrace();
         }
+        
      }
+}
